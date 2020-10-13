361e6f2bf686a61a9d8cc1fda075e6b3acaa62ea
Marc Te <marcte42@gmail.com>
2020-10-13 12:14:36 +0200

    Updated syntax

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: mterkhoy <mterkhoy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 12:44:45 by mterkhoy          #+#    #+#             */
-/*   Updated: 2020/10/09 12:49:17 by mterkhoy         ###   ########.fr       */
+/*   Updated: 2020/10/13 12:13:05 by mterkhoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ char	*ft_strcpy(char * dst, const char * src)
 
 	i = -1;
 	while (src[++i])
-	{
 		dst[i] = src[i];
-	}
 	dst[i] = '\0';
 	return (dst);
 }
