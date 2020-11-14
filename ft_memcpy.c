bed2575e9b27eb8ebcc4227aaddb4f1992b5982e
Marc Te <marcte42@gmail.com>
2020-11-14 20:56:20 +0100

    Removed protection from null pointers to match the original function.

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mterkhoy <mterkhoy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 17:55:26 by mterkhoy          #+#    #+#             */
-/*   Updated: 2020/10/09 16:11:27 by mterkhoy         ###   ########.fr       */
+/*   Updated: 2020/11/14 20:55:16 by marc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
-	if (!(dst) && !(src))
-		return (0);
 	i = 0;
 	while (i < n)
 	{
