d71d90696870fb39fa3acd40836acdcc3c92dc6d
Marc Te <marcte42@gmail.com>
2020-11-16 14:27:28 +0100

    memcpy protected from NULL pointers

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mterkhoy <mterkhoy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 17:55:26 by mterkhoy          #+#    #+#             */
-/*   Updated: 2020/11/14 20:55:16 by marc             ###   ########.fr       */
+/*   Updated: 2020/11/16 14:26:26 by mterkhoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
+	if (!dst && !src)
+		return (0);
 	i = 0;
 	while (i < n)
 	{
